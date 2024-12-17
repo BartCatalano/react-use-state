975612e711f2772a021657fd7671414ae9bb3ee6
BartCatalano <catalanobartolomeo@gmail.com>
2024-12-17 10:06:32 +0100

    feat: push sistemato

--- src/components/AppButton.jsx
+++ src/components/AppButton.jsx
@@ -7,13 +7,11 @@ import languages from "../date/languages";
 function AppButton() {
 const [activeLang, setActiveLang] = useState ("Clicca su un linguaggio per avere la descrizione")
 
-const changeLanguages = (newLanguages) => {
-setActiveLang(newLanguages);
-}
+
     return (
         <div>
        {languages.map((curLang, id ) => (
-            <button onClick={() => {changeLanguages(curLang.description)}} key={curLang.id}> {curLang.title}</button>
+            <button onClick={() => {setActiveLang(curLang.description)}} key={curLang.id}> {curLang.title}</button>
          ))}
          <div className="border">
            
